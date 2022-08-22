8dedf416b84e64bd429e4d5edc766b2c782f6341
Cristian <89285428+Cristiangonzz@users.noreply.github.com>
2022-08-22 19:26:29 -0300

    Añadiendo clases y controladores

---
+++ src/Clases/Artista.java
@@ -0,0 +1,48 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Clases;
+/**
+ *
+ * @author Cristian
+ */
+public class Artista extends Usuarios {
+    private String Descripcion;
+    private String Biografia;
+    private String URL;
+
+    public Artista(String Nickname, String Clave, String Nombre, String Apellido, String Mail,String Descripcion,String Biografia,String URL) {
+        super(Nickname,Clave,Nombre,Apellido,Mail);
+        this.Descripcion = Descripcion;
+        this.Biografia = Biografia;
+        this.URL = URL;
+    }
+
+    public String getDescripcion() {
+        return Descripcion;
+    }
+
+    public void setDescripcion(String Descripcion) {
+        this.Descripcion = Descripcion;
+    }
+
+    public String getBiografia() {
+        return Biografia;
+    }
+
+    public void setBiografia(String Biografia) {
+        this.Biografia = Biografia;
+    }
+
+    public String getURL() {
+        return URL;
+    }
+
+    public void setURL(String URL) {
+        this.URL = URL;
+    }
+    
+    
+}
---
+++ src/Servicios/Servicios.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Servicios;
+
+import Clases.Usuarios;
+
+/**
+ *
+ * @author Cristian
+ */
+public class Servicios {
+    
+    public Servicios(){
+        
+    }
+    
+    public Usuarios getusuario(){
+        return null;
+        
+    }
+    
+    
+}
